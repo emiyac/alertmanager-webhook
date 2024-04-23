511a4bd9117ed6bcdddde1de74b23680d3a9d088
emiya <945695227@qq.com>
2024-04-23 15:50:24 +0800

    fix: 修复jinja2自动转义url导致url不可用的问题

--- main.py
+++ main.py
@@ -18,36 +18,38 @@ def health_check():
 
 @app.post("/wx")
 async def send_msg_to_wx(items: AlertManagerModel):
+    logger.debug("items is: {}".format(items))
     if not settings.WX_WEBHOOK:
         raise Exception("WX_WEBHOOK is not set")
     content = templates.get_template(settings.WX_TEMPLATE).render(items=items, grafana_url=grafana_url)
-    logger.debug(content)
+    logger.debug("content is: {}".format(content))
     data = {"msgtype": "markdown", "markdown": {"content": content}}
     
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url=settings.WX_WEBHOOK, json=data, timeout=timeout) as resp:
-            resp_code = resp.status
-            resp_data = await resp.json()
-            logger.info("code={}, result={}".format(resp_code, resp_data))
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post(url=settings.WX_WEBHOOK, json=data, timeout=timeout) as resp:
+    #         resp_code = resp.status
+    #         resp_data = await resp.json()
+    #         logger.info("code={}, result={}".format(resp_code, resp_data))
     return {"code": "0000", "data": None}
 
 
 @app.post("/dingtalk")
 async def send_msg_to_dingtalk(items: AlertManagerModel):
+    logger.debug("items is: {}".format(items))
     if not settings.DINGTALK_WEBHOOK or not settings.DINGTALK_SECRET:
         raise Exception("DINGTALK_WEBHOOK or DINGTALK_SECRET is not set")
     content = templates.get_template(settings.DINGTALK_TEMPLATE).render(items=items, grafana_url=grafana_url)
-    logger.debug(content)
+    logger.debug("content is: {}".format(content))
     timestamp, sign = get_sign(settings.DINGTALK_SECRET)
     url = "{}&timestamp={}&sign={}".format(settings.DINGTALK_WEBHOOK, timestamp, sign)
     title = "自定义告警"
     data = {"msgtype": "markdown", "markdown": {"title": title, "text": content}}
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url=url, json=data, timeout=timeout) as resp:
-            resp_code = resp.status
-            resp_data = await resp.json()
-            logger.info("code={}, result={}".format(resp_code, resp_data))
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post(url=url, json=data, timeout=timeout) as resp:
+    #         resp_code = resp.status
+    #         resp_data = await resp.json()
+    #         logger.info("code={}, result={}".format(resp_code, resp_data))
     return {"code": "0000", "data": None}
 
 
