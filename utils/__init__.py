66ede988c2ad193f19e7897fc46c7610f56c54e4
emiya <945695227@qq.com>
2024-04-26 16:13:03 +0800

    refactor: formatter code use ruff

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,16 @@
-from .config import LOGGING_CONFIG, settings
+from .config import LOGGING_CONFIG, base_dir, settings
 from .logger import logger
 from .salt import get_sign
 from .schema import AlertManagerModel
 
-__all__ = ["LOGGING_CONFIG", "settings", "logger", "get_sign", "AlertManagerModel"]
+__all__ = [
+    "LOGGING_CONFIG",
+    "settings",
+    "logger",
+    "base_dir",
+    "get_sign",
+    "AlertManagerModel",
+]
 
 for key, value in vars(settings).items():
     logger.info(f"{key}: {value}")
